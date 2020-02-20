8bf1765c30076ba1a82426fef8e904af994ca5a4
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-19 20:53:47 -0600

    Add my_each_with_index function.

--- public/main.rb
+++ public/main.rb
@@ -7,4 +7,14 @@ module Enumerable
       yield(indx)
     end
   end
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+
+    counter = 0
+    for indx in self do
+      yield(indx, counter)
+      counter += 1
+    end
+  end
 end
