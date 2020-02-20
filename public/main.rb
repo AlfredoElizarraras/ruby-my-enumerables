1c0916138c60a0ea194176241fc49fa7556e75c3
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-19 21:50:25 -0600

    Add my_all? function.

--- public/main.rb
+++ public/main.rb
@@ -25,4 +25,23 @@ module Enumerable
     self.my_each {|indx| arr.push(indx) if yield(indx) }
     arr
   end
+
+  def my_all?
+    return_value = true
+    return return_value unless block_given?
+
+    self.my_each do |indx| 
+      return_value = yield(indx)
+      break unless return_value
+    end
+    return_value
+  end
 end
+
+
+p %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+p %w[ant bear cat].all?(/t/)                        #=> false
+p [1, 2i, 3.14].all?(Numeric)                       #=> true
+p [nil, true, 99].all?                              #=> false
+p [].all?  
