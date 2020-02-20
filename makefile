a2a99c1d247cf33eae0e1898f12b58f15c6a52eb
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-20 10:06:43 -0600

    Add my_none method.

--- makefile
+++ makefile
@@ -1,9 +1,15 @@
-git:
+g:
 	git add .
 	git commit -m "$m"
+ifeq ($b, f)
+	git flow feature finish $f
+endif
 ifeq ($b, b)
 	git flow bugfix finish $f
 endif
-ifeq ($b, f)
-	git flow bugfix finish $f
+ifeq ($b, r)
+	git flow release finish $f
+endif
+ifeq ($b, h)
+	git flow hotfix finish $f
 endif
--- public/main.rb
+++ public/main.rb
@@ -53,4 +53,28 @@ module Enumerable
     end
     return_value
   end
+
+  def my_none?(eval = nil)
+    return_value = false
+
+    if block_given?
+      self.my_each do |indx|
+        return_value = !yield(indx)
+        break if return_value
+      end
+    else
+      return_value = self.my_all? { |obj| eval === obj }
+    end
+    return_value
+  end
 end
+
+
+p %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+p %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+p %w{ant bear cat}.none?(/d/)                        #=> true
+p [1, 3.14, 42].none?(Float)                         #=> false
+p [].none?                                           #=> true
+p [nil].none?                                        #=> true
+p [nil, false].none?                                 #=> true
+p [nil, false, true].none?                           #=> false
