f39fba19ca01e984ca3633c04894dc686cc790a7
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-19 21:29:09 -0600

    Add my_select function.

--- public/main.rb
+++ public/main.rb
@@ -9,12 +9,20 @@ module Enumerable
   end
 
   def my_each_with_index
-    return to_enum(:my_each) unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     counter = 0
     for indx in self do
       yield(indx, counter)
       counter += 1
     end
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    arr = []
+    self.my_each {|indx| arr.push(indx) if yield(indx) }
+    arr
   end
 end
