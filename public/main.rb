9260e46922eb97d828b561a1f27782979b49a36c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-02-21 09:10:22 -0600

    Make improvements of style accord with rubocop.

--- public/main.rb
+++ public/main.rb
@@ -1,5 +1,6 @@
+# rubocop: disable Style/CaseEquality
+# rubocop: disable Metrics/ModuleLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
-# Each
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
@@ -23,21 +24,21 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     arr = []
-    self.my_each { |indx| arr.push(indx) if yield(indx) }
+    my_each { |indx| arr.push(indx) if yield(indx) }
     arr
   end
 
-  def my_all?( eval = nil )
+  def my_all?(eval = nil)
     return_value = true
 
     if block_given?
-      self.my_each do |indx| 
+      my_each do |indx|
         return_value = yield(indx)
         break unless return_value
       end
     else
-      return_value = self.my_all? { |obj| eval === obj }
-    end 
+      return_value = my_all? { |obj| eval === obj }
+    end
     return_value
   end
 
@@ -45,12 +46,12 @@ module Enumerable
     return_value = false
 
     if block_given?
-      self.my_each do |indx|
+      my_each do |indx|
         return_value = yield(indx)
         break if return_value
       end
     else
-      return_value = self.my_all? { |obj| eval === obj }
+      return_value = my_all? { |obj| eval === obj }
     end
     return_value
   end
@@ -59,24 +60,24 @@ module Enumerable
     return_value = false
 
     if block_given?
-      self.my_each do |indx|
+      my_each do |indx|
         return_value = !yield(indx)
         break if return_value
       end
     else
-      return_value = self.my_all? { |obj| eval === obj }
+      return_value = my_all? { |obj| eval === obj }
     end
     return_value
   end
 
   def my_count(*eval)
     return_value = 0
-    sum_one = -> (bool) { return_value += 1 if bool }
+    sum_one = ->(bool) { return_value += 1 if bool }
 
     if block_given?
-      self.my_each { |indx| sum_one.call(yield(indx)) }
+      my_each { |indx| sum_one.call(yield(indx)) }
     else
-      self.my_each { |indx| eval.empty? ? return_value += 1 : sum_one.call(eval[0] == indx) }
+      my_each { |indx| eval.empty? ? return_value += 1 : sum_one.call(eval[0] == indx) }
     end
 
     return_value
@@ -86,7 +87,7 @@ module Enumerable
     return to_enum(:my_map) unless block_given?
 
     arr = []
-    self.my_each { |indx| arr.push(yield(indx)) }
+    my_each { |indx| arr.push(yield(indx)) }
     arr
   end
 
@@ -94,22 +95,24 @@ module Enumerable
     memo = nil
     sym = nil
 
-    is_symbol_number = -> (value) do
-      if Symbol === value || String === value 
-          sym = value.to_sym
+    is_symbol_number = lambda do |value|
+      if Symbol === value || String === value
+        sym = value.to_sym
       elsif Numeric === args[0] && block_given?
         memo = args[0]
       else
         raise TypeError, "#{value} is not a symbol nor a string"
-      end  
+      end
     end
 
-    do_loop = -> do
-      self.my_each { |indx| memo = memo.nil? ? indx : block_given? ? yield(memo,indx) : memo.send(sym, indx) }
+    do_loop = lambda do
+      my_each { |indx| memo = memo.nil? ? indx : block_given? ? yield(memo, indx) : memo.send(sym, indx) }
     end
 
-    if args.my_count == 1 
-      is_symbol_number.call(args[0]) unless block_given?
+    if args.my_count == 1
+      unless block_given?
+        is_symbol_number.call(args[0])
+      end
     elsif args.my_count == 2
       is_symbol_number.call(args[1])
     end
@@ -117,7 +120,6 @@ module Enumerable
 
     memo
   end
-
 end
 
 def multiply_els(arr)
@@ -125,4 +127,7 @@ def multiply_els(arr)
   x = array.my_inject(:*)
 end
 
-p multiply_els([2,4,5])
+p multiply_els([2, 4, 5])
+
+# rubocop: enable Style/CaseEquality
+# rubocop: enable Metrics/ModuleLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
